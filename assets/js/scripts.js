8b4706cebca5262c855e8951c8f89df7c57139f8
Sergey-Okey <sergeyborisov_1@vk.com>
2024-01-13 12:14:00 +0300

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,4 +20,41 @@ document.addEventListener('DOMContentLoaded', function () {
 	closeSidebar();
 });
 
-//CaruselPortfolio
+//Active Link Header
+function activateLink(link) {
+	var links = document.querySelectorAll('.menu a'); // Получаем все ссылки в меню
+	links.forEach(function (item) {
+		item.classList.remove('active'); // Удаляем класс "active" у всех ссылок
+	});
+	link.classList.add('active'); // Добавляем класс "active" к ссылке, на которую нажали
+}
+
+//Skills Animation
+
+document.addEventListener("DOMContentLoaded", function () {
+	var items = document.querySelectorAll(".portfolio_item");
+
+	function checkVisibility() {
+		items.forEach(function (item, index) {
+			var position = item.getBoundingClientRect();
+			var windowCenter = window.innerHeight / 2;
+
+			// Проверяем, виден ли элемент
+			if (position.top < windowCenter && position.bottom > windowCenter) {
+				// Применяем анимацию
+				item.style.transition = "opacity 0.9s, transform 0.5s";
+				item.style.opacity = 1;
+				item.style.transform = "translateX(0)";
+			} else {
+				// Возвращаем не видимые элементы в центр
+				item.style.transition = "none";
+				item.style.opacity = 0;
+				item.style.transform = index % 2 === 0 ? "translateX(-50%)" : "translateX(50%)";
+			}
+		});
+	}
+
+	// Вызываем функцию при загрузке страницы и при прокрутке
+	window.addEventListener("scroll", checkVisibility);
+	checkVisibility();
+});
