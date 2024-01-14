c2b07bcf721780d8ceb014a2411089f73f2d59e0
Sergey-Okey <sergeyborisov_1@vk.com>
2024-01-14 14:06:46 +0300

    update finish

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -108,17 +108,6 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 //Scrol To Top
-window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-	var button = document.getElementById("button_scroll");
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		button.style.display = "block";
-	} else {
-		button.style.display = "none";
-	}
-}
-
 function scrollToTop() {
 	var currentPosition = document.documentElement.scrollTop || document.body.scrollTop;
 
